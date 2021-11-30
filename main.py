4a8643a9de198cf0dd03974afa1071b9c646b0b6
michiAkaWhitey <weiss_michael@a1.net>
2021-11-30 11:49:00 +0100

    Project finished

--- main.py
+++ main.py
@@ -18,14 +18,13 @@ correct function depends on the target
 gridSize = (12, 8)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, corners = cv2.findCirclesGrid(gray, gridSize, cv2.CALIB_CB_ASYMMETRIC_GRID)
-print(ret)
 if ret == True: # target found
     # define stopping criteria for refining the grid
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     corners2 = cv2.cornerSubPix(gray, corners, gridSize, (-1, -1), criteria)
 
     featured_img = cv2.drawChessboardCorners(img, gridSize, corners, ret)
+    cv2.imwrite('featured_target.jpg', featured_img)
     cv2.imshow('img', img)
     cv2.waitKey(0)
 
-    cv2.imwrite('featured_target.jpg', featured_img)
